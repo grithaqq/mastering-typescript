954cd62b3bf330d2e4867a65e7f87dd6e7a3160d
grithaqq <grithaqq@gmail.com>
2024-06-05 15:05:30 +0800

    for loop exercise

---
+++ src/looping/exercise.ts
@@ -0,0 +1,18 @@
+let list_items = [
+    {name: "Jolly Tisue", price: 10},
+    {name: "Energen", price: 5},
+    {name: "Indomie Original", price: 5},
+    {name: "Sari roti", price: 5},
+];
+
+// calculate all items price
+
+let items = [];
+let total: number = 0;
+
+for (var item of list_items){
+    items.push(item.name);
+    total+= item.price;
+}
+console.log(`Your list items : ${items}`);
+console.log(`total: $${total}`)
