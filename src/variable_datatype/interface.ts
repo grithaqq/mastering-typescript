946516d736356251908b258599d8be7d33bec746
grithaqq <grithaqq@gmail.com>
2024-06-06 22:11:55 +0800

    Create new interaface

---
+++ src/variable_datatype/interface.ts
@@ -0,0 +1,47 @@
+// interface
+
+interface Person {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+let afif: Person ={
+    name: "Afif",
+    age: 26,
+    gender: "male"
+};
+
+let olan: Person = {
+    name: "olan",
+    age: 27,
+    gender: "male"
+};
+
+console.log({afif, olan});
+console.info(olan)
+
+enum PlayerRole {
+    SETTER,
+    OUTSIDE_HITTER,
+    OPPOSITE_HITTER,
+    LIBERO,
+    MIDLE_BLOCK,
+}
+
+
+interface Player {
+    name: string,
+    role: PlayerRole,
+    hight: number,
+    weight: number
+}
+
+const hinata: Player = {
+    name: "Hinata Shoyo",
+    role: PlayerRole.MIDLE_BLOCK,
+    hight: 164.2,
+    weight: 51.9
+}
+
+console.log({hinata})
